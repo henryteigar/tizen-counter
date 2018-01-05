3224e928fd1bf9dedf20b62b6007159ae18ebce5
karlveskus <karl.veskus@gmail.com>
2018-01-05 14:48:56 -0800

    Add localStorage for count storing

--- js/app.model.js
+++ js/app.model.js
@@ -4,57 +4,62 @@ window.app = window.app || {};
 
 (function(app) {
 	
-	var currentBadgeCount = 0,
-		countText = null,
+	var countText = null,
 		applicationId = '';
 	
-	function getApplicationId() {
-        try {
-            return tizen.application.getCurrentApplication().appInfo.id;
-        } catch (error) {
-            console.error('GetCurrentApplication id error: ', error);
-        }
+//	function getApplicationId() {
+//        try {
+//            return tizen.application.getCurrentApplication().appInfo.id;
+//        } catch (error) {
+//            console.error('GetCurrentApplication id error: ', error);
+//        }
+//    }
+//	
+//	function getBadgeCount() {
+//        try {
+//        	return tizen.badge.getBadgeCount(applicationId);
+//        } catch (error) {
+//            console.error('getBadgeCount error: ', error);
+//        }
+//    }
+//	
+//	function updateBadgeCount(value) {
+//		changeBadgeCount(value);
+//	}
+//	
+//	function changeBadgeCount(newValue) {
+//        try {
+//        	tizen.badge.setBadgeCount(applicationId, newValue);
+//        } catch (error) {
+//        	tizen.badge.setBadgeCount(applicationId, 0);
+//        }
+//	}
+	
+	function getCurrentCount() {
+		var count = localStorage.getItem("count");
+		
+		if (count !== null) {
+			return count;
+		} else {
+			return 0;
+		}
     }
 	
-	function getBadgeCount() {
-        try {
-        	return tizen.badge.getBadgeCount(applicationId);
-        } catch (error) {
-            console.error('getBadgeCount error: ', error);
-        }
-    }
-	
-	function updateBadgeCount(value) {
-		changeBadgeCount(value)
+	function updateCount(count) {
+		localStorage.setItem("count", count);
 	}
-	
-	
-	
-	function changeBadgeCount(newValue) {
-        try {
-        	tizen.badge.setBadgeCount(applicationId, newValue)
-        } catch (error) {
-        	tizen.badge.setBadgeCount(applicationId, 0)
-        }
-
-	}
-	
-	function getCurrentBadgeCount() {
-        return currentBadgeCount;
-    }
 	
 	function init() {
-		countText = document.getElementById('count-number');
-        applicationId = getApplicationId();
-        currentBadgeCount = getBadgeCount();
+//        applicationId = getApplicationId();
+//        currentBadgeCount = getBadgeCount();
         document.getElementById('loading').style.display = "none";
         //document.getElementsByClassName('content')[0].style.display = "block";
     }
 
     app.model = {
         init: init,
-        updateBadgeCount: updateBadgeCount,
-        getCurrentBadgeCount: getCurrentBadgeCount
+        updateCount: updateCount,
+        getCurrentCount: getCurrentCount
     };
 
 	
--- js/app.ui.js
+++ js/app.ui.js
@@ -6,26 +6,36 @@ window.app = window.app || {};
 		increaseButton = null,
 		decreaseButton = null
 	
-	function increaseCount() {
-		var newCount = parseInt(countText.innerHTML) + 1;
-		//countText.style.fontSize = "20px";
-		countText.innerHTML = newCount;
-	}
-	
-	function decreaseCount() {
-		var newCount = parseInt(countText.innerHTML) - 1;
-		countText.innerHTML = newCount;
-	}
+//	function increaseCount() {
+//		var newCount = parseInt(countText.innerHTML) + 1;
+//		//countText.style.fontSize = "20px";
+//		countText.innerHTML = newCount;
+//	}
+//	
+//	function decreaseCount() {
+//		var newCount = parseInt(countText.innerHTML) - 1;
+//		countText.innerHTML = newCount;
+//	}
 	
 	function onIncreaseButtonClick() {
-		increaseCount()
+		var count = app.model.getCurrentCount();
+		var newCount = +(count) + 1;
+		app.model.updateCount(newCount);
+		updateCount(newCount)
 	}
 	
 	function onDecreaseButtonClick() {
-		console.log(screen.height)
+//		console.log(screen.height)
 		//console.log(tizen.systeminfo.getCapability("http://tizen.org/feature/systemInfoDisplay/resolutionWidth"))
-		decreaseCount()
+		var count = app.model.getCurrentCount();
+		var newCount = +(count) - 1
+		app.model.updateCount(newCount);
+		updateCount(newCount)
     }
+	
+	function updateCount(count) {
+		countText.innerHTML = count;
+	}
 	
 	function bindUiEvents() {
 		increaseButton.addEventListener('click', onIncreaseButtonClick);
@@ -36,26 +46,27 @@ window.app = window.app || {};
 		document.addEventListener('rotarydetent', function(ev) {
 		    var direction = ev.detail.direction;
 		    if (direction == 'CW') {
-		    	increaseCount();
+		    	onIncreaseButtonClick();
 		    } else if (direction == 'CCW') {
-		    	 decreaseCount();
+		    	onDecreaseButtonClick();
 		    }
 		});
 	}
 	
-	function updateBadgeInBackground() {
-		if (countText) {
-			app.model.updateBadgeCount(countText.innerHTML);
-		}
-	}
-	
+//	function updateBadgeInBackground() {
+//		if (countText) {
+//			app.model.updateBadgeCount(countText.innerHTML);
+//		}
+//	}
+//	
 	function init() {
 		countText = document.getElementById('count-number');
 		increaseButton = document.getElementById("increase-btn");
 		decreaseButton = document.getElementById("decrease-btn");
-		countText.innerText = app.model.getCurrentBadgeCount();
-		setInterval(updateBadgeInBackground, 200);
+		console.log(app.model.getCurrentCount())
 		bindUiEvents();
+		countText.innerHTML = app.model.getCurrentCount();
+//		setInterval(updateBadgeInBackground, 200);
 		addRotaryEventHandler();
 	}
 	
